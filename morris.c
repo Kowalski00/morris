11fdd13c7fe4ad14583d3f517ed0d703b2856d4b
kowalski00 <rk0191@hotmail.com>
2024-04-29 20:47:24 -0300

    Starts building logic to set colors

--- morris.c
+++ morris.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <windows.h>
 
 #define ROW_SIZE 1024
 
@@ -7,52 +10,46 @@ const char *n_desc[] = {
     "Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"
 };
 
-void printArray(int *array, char *symbol) {
-    printf(symbol);
-    for(int i = 0; i < sizeof(array) / sizeof(array[0]); i++ ) {
-        printf(" %d",array[i]);
-    }
-}
-
-void printArrayAndFilterZeros(int *array, size_t len) {
+void printLine(int lineNumber, int *array, size_t len, HANDLE handleConsole, WORD color, WORD savedAttributes)
+{
     unsigned int *dst = array;
-    for(int i = 0; i < len; i++ ) {
-        if(*dst != 0) printf(" %d",*dst);
-        dst++;
-    }
-}
-
-void printLine(int lineNumber, int *array, size_t len) {
-
     printf("\n %3d |  ", lineNumber);
-    unsigned int *dst = array;
-    for(int i = 0; i < len; i++ ) {
+    SetConsoleTextAttribute(handleConsole, color);
+    for(int i = 0; i < len; i++ )
+    {
         if(*dst != 0) printf(" %d",*dst);
         dst++;
     }
+    SetConsoleTextAttribute(handleConsole, savedAttributes);
 }
 
-void clearArray(int *array, size_t len) {
+void clearArray(int *array, size_t len)
+{
     unsigned int *dst = array;
-    for(int i = 0; i < len; i++ ) {
+    for(int i = 0; i < len; i++ )
+    {
         *dst = 0;
         dst++;
     }
 }
 
-void copyArray(int *src, int *destiny, size_t len) {
+void copyArray(int *src, int *destiny, size_t len)
+{
     unsigned int *psrc = src;
     unsigned int *pdst = destiny;
-    for(int i = 0; i < len; i++ ) {
+    for(int i = 0; i < len; i++ )
+    {
         *pdst = *psrc;
         pdst++;
         psrc++;
     }
 }
 
-void enrichRow(int digitQuantity, int digit, int *array) {
+void enrichRow(int digitQuantity, int digit, int *array)
+{
     unsigned int *dst = array;
-    for(int i = 0; i < ROW_SIZE; i++ ) {
+    for(int i = 0; i < ROW_SIZE; i++ )
+    {
         if(*dst == 0) {
             *dst = digitQuantity;
             *dst++;
@@ -62,11 +59,11 @@ void enrichRow(int digitQuantity, int digit, int *array) {
     }
 }
 
-void printLineDescription(int lineNumber, int *array, size_t len) {
-
+void printLineDescription(int lineNumber, int *array, size_t len)
+{
     printf("\n     |    --> ");
-    for(int i=0; i < len; i+=2) {
-
+    for(int i=0; i < len; i+=2)
+    {
         if(*array == 0) continue;
 
         printf("%s ", n_desc[*array++]);
@@ -76,8 +73,8 @@ void printLineDescription(int lineNumber, int *array, size_t len) {
     }
 }
 
-int main() {
-
+int main()
+{
     int digitGroupQuantity = 0;
     int currentRow[ROW_SIZE];
     int nextRow[ROW_SIZE];
@@ -85,10 +82,17 @@ int main() {
     int showDescription = 0;
     int lineLimit;
     int firstNumber;
+    int lastColorKey = 0;
+    int colorKey = 0;
+
+    HANDLE handleConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    GetConsoleScreenBufferInfo(handleConsole, &consoleInfo);
+    WORD savedAttributes = consoleInfo.wAttributes;
 
     printf("Do you want to show description below each lines? (1 or 0) \n");
     scanf("%d", &showDescription);
-    
+
     printf("At what line should the sequence stop? (Insert a number or -1 to limit to 100) \n");
     scanf("%d", &lineLimit);
 
@@ -101,20 +105,20 @@ int main() {
     clearArray( nextRow, ROW_SIZE );
     currentRow[0] = firstNumber;
 
-    printLine(line, currentRow, ROW_SIZE);
+    printLine(line, currentRow, ROW_SIZE, handleConsole, FOREGROUND_BLUE, savedAttributes);
     
-    while(line < lineLimit) {
-
+    while(line < lineLimit)
+    {
         int currentDigit = currentRow[0];
         int lastDigit = currentRow[0];
 
-        for(int r = 0; r < ROW_SIZE ; r++ ) {
-
+        for(int r = 0; r < ROW_SIZE ; r++ )
+        {
             currentDigit = currentRow[r];
 
             if(lastDigit != currentDigit) {
 
-                enrichRow(digitGroupQuantity,lastDigit,nextRow); // NOTE: Why this function making compile/execution taking so long to execute
+                enrichRow(digitGroupQuantity,lastDigit,nextRow);
 
                 digitGroupQuantity = 0;
 
@@ -125,14 +129,18 @@ int main() {
             digitGroupQuantity++;
 
             lastDigit = currentDigit;
-
         }
 
         copyArray(nextRow, currentRow, ROW_SIZE);
-        
         clearArray( nextRow, ROW_SIZE );
 
-        printLine(line+1, currentRow, ROW_SIZE);
+        do{
+            colorKey =  rand() % 4;
+        } while (colorKey == lastColorKey);
+        
+        WORD wColor = (FOREGROUND_BLUE);
+        printLine(line+1, currentRow, ROW_SIZE, handleConsole, wColor, savedAttributes);
+
         if(showDescription) printLineDescription(line+1, currentRow, ROW_SIZE);
         line++;
     }
